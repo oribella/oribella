20fa93ae8d45533d0bf36e130a077e35791bce89
stoffeastrom <stoffeastrom@gmail.com>
2017-02-12 22:12:19 +0100

    chore(package): update dependencies

--- packages/oribella/webpack.config.js
+++ packages/oribella/webpack.config.js
@@ -1,4 +1,5 @@
-import path = require('path');
+const path = require('path');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
   entry: './src/oribella.ts',
@@ -15,9 +16,18 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.ts$/, use: [{ loader: 'awesome-typescript-loader' }] },
+      { test: /\.ts$/, use: [{ loader: 'awesome-typescript-loader', query: { configFileName: './tsconfig.build.json', declaration: false } }] },
       { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' }
     ]
   },
+  plugins: [
+    new CompressionPlugin({
+      asset: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: /\.js$|\.map$/,
+      threshold: 10240,
+      minRatio: 0.8
+    })
+  ],
   devtool: 'source-map'
 };
