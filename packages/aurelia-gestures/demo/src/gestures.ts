2b8a9e85ada1ba2e30a182408d73aebe1be8396a
stoffeastrom <stoffeastrom@gmail.com>
2017-02-22 21:23:19 +0100

    chore(lint): update

--- packages/aurelia-gestures/demo/src/gestures.ts
+++ packages/aurelia-gestures/demo/src/gestures.ts
@@ -3,18 +3,23 @@ import { SwipeData } from 'oribella';
 
 export class Gestures {
   public onTap(args: DefaultListenerArgs) {
+    // tslint:disable-next-line:no-console
     console.log('tap', args);
   }
   public onDoubletap(args: DefaultListenerArgs) {
+    // tslint:disable-next-line:no-console
     console.log('doubletap', args);
   }
   public onLongtap(args: DefaultListenerArgs) {
+    // tslint:disable-next-line:no-console
     console.log('longtap', args);
   }
   public onSwipe(args: ListenerArgs<SwipeData>) {
+    // tslint:disable-next-line:no-console
     console.log('swipe', args);
   }
   public onLongtapSwipe(args: ListenerArgs<SwipeData>) {
+    // tslint:disable-next-line:no-console
     console.log('longtap swipe', args);
   }
 }
