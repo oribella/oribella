724881d0235307504e5bd069cb87775e7852b19c
stoffeastrom <stoffeastrom@gmail.com>
2017-03-07 19:36:42 +0100

    fix(oribella-aurelia-gestures): import and export `gestures.ts` to make bundlers happy

--- packages/aurelia-gestures/src/oribella-aurelia-gestures.ts
+++ packages/aurelia-gestures/src/oribella-aurelia-gestures.ts
@@ -1,6 +1,9 @@
 import { FrameworkConfiguration } from 'aurelia-framework';
 import { PLATFORM } from 'aurelia-pal';
+import * as G from './gestures';
 
 export function configure(config: FrameworkConfiguration) {
   config.globalResources(PLATFORM.moduleName('./gestures'));
 }
+
+export { G };
