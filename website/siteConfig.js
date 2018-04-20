d3b14e187a8f07b87474875e3c77933fd5ff0c38
Christoffer Åström <stoffeastrom@gmail.com>
2018-04-20 10:55:56 +0200

    docs: add algolia

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -31,13 +31,13 @@ const siteConfig = {
     'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
   ],
   repoUrl: 'https://github.com/oribella/oribella',
-  // algolia: {
-  //   apiKey: 'f828f1413be7483525f2eee73193dd5d',
-  //   indexName: 'oribella',
-  //   algoliaOptions: {
-  //     hitsPerPage: 7,
-  //   },
-  // },
+  algolia: {
+    apiKey: 'ddaa175b4947881d3a2270dc6e5cd945',
+    indexName: 'oribella',
+     algoliaOptions: {
+     hitsPerPage: 7,
+    },
+  },
 };
 
 module.exports = siteConfig;
